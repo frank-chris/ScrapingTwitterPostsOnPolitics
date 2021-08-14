4cc8d5a35e1c9e8600eaa4db8c5f81330bbfa009
Tom Glint <34976510+TomGlint@users.noreply.github.com>
2021-08-14 21:29:54 +0530

    Adding a script to convert the json files to csv files with tab delimitation

---
+++ glintWS/scripts/convertJSONFileToCSV.py
@@ -0,0 +1,28 @@
+import json
+import csv
+
+
+#input data file containing all the stuff
+dataFileName = "test.csv"
+data = []
+outputFileName = "data_file.csv"
+
+with open (dataFileName) as f:
+        for line in f:
+            data.append(json.loads(line))
+
+
+# now we will open a file for writing
+data_file = open(outputFileName, 'w')
+ 
+
+# create the csv writer object
+csv_writer = csv.writer(data_file, delimiter ='\t',quoting=csv.QUOTE_MINIMAL)
+print("Printing headers for inspection")
+print(data[0].keys())
+
+csv_writer.writerow(data[0].keys())
+
+for row in data:
+    csv_writer.writerow(row.values())
+data_file.close()
